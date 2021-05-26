eb305e74d3a5e25e0c94aee2984b502effb46942
minhle0105 <minh.anh.le.cnn@gmail.com>
2021-05-27 01:54:40 +0700

    Remove unused import in auth controller

--- src/main/java/com/codeforvn/controller/AuthController.java
+++ src/main/java/com/codeforvn/controller/AuthController.java
@@ -3,7 +3,6 @@ package com.codeforvn.controller;
 import com.codeforvn.dto.RegisterRequest;
 import com.codeforvn.service.authentication.AuthService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
