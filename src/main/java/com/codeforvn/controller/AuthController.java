4f2e8481b026a34d6cd8f2bf37681c07b1c590b1
minhle0105 <minh.anh.le.cnn@gmail.com>
2021-05-27 01:54:18 +0700

    Change to allargs, remove autowired annotation in auth controller

--- src/main/java/com/codeforvn/controller/AuthController.java
+++ src/main/java/com/codeforvn/controller/AuthController.java
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
+@AllArgsConstructor
 public class AuthController {
 
-    @Autowired
-    private AuthService authService;
+    private final AuthService authService;
 
     @PostMapping("/signup")
     public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
