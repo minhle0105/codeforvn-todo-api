4d16f196b8f8e0537401d255d568cb5fce32ff53
minhle0105 <minh.anh.le.cnn@gmail.com>
2021-05-27 04:19:11 +0700

    Cannot get private key

--- src/main/java/com/codeforvn/security/JwtProvider.java
+++ src/main/java/com/codeforvn/security/JwtProvider.java
@@ -10,6 +10,7 @@ import com.codeforvn.model.User;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.*;
@@ -22,13 +23,26 @@ import static java.util.Date.from;
 public class JwtProvider {
 
     private KeyStore keyStore;
+    private final char[] pwdArray = "secret".toCharArray();
 
+    public void createKeyStore() {
+        try {
+            keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            keyStore.load(null, pwdArray);
+            FileOutputStream fos = new FileOutputStream("newKeyStore.jks");
+            keyStore.store(fos, pwdArray);
+        }
+        catch (KeyStoreException | CertificateException | IOException | NoSuchAlgorithmException e) {
+            throw new TodolistException("Exception occurred while creating keystore");
+        }
+    }
     @PostConstruct
     public void init() {
         try {
+            createKeyStore();
             keyStore = KeyStore.getInstance("JKS");
-            InputStream resourceAsStream = getClass().getResourceAsStream("/springblog.jks");
-            keyStore.load(resourceAsStream, "secret".toCharArray());
+            InputStream resourceAsStream = getClass().getResourceAsStream("newKeyStore.jks");
+            keyStore.load(resourceAsStream, pwdArray);
         } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException e) {
             throw new TodolistException("Exception occurred while loading keystore");
         }
@@ -44,7 +58,7 @@ public class JwtProvider {
 
     private Key getPrivateKey() {
         try {
-            return (PrivateKey) keyStore.getKey("springblog","secret".toCharArray());
+            return (PrivateKey) keyStore.getKey("newKeyStore","secret".toCharArray());
         } catch (KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
             throw new TodolistException("Exception occurred while retrieving public key from keystore");
         }
